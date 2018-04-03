66894815c11b66df106ffc6abb83950ef04d719a
Brian <riceb53@gmail.com>
2018-04-02 19:59:16 -0500

    for loop demo

---
+++ intro.js
@@ -0,0 +1,18 @@
+// console.log('hello');
+
+// i = 0
+// while i < 10
+//   p i
+//   i += 1
+// end
+
+// simple 'for' loop
+// for (var i = 0; i < 10; i++) {
+//   console.log(i);
+// }
+
+var fruits = ["apple", "banana", "pear", "tomato"];
+
+for (var i = 0; i < fruits.length; i--) {
+  console.log(fruits[i]);
+}
